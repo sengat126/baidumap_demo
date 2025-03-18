d15e630f55ebfc1b8cca535f8587ceceaeb3f4cd
sengat126 <sengat126@sina.com>
2025-03-18 16:53:30 +0800

    百度地图v3_标记demo初始化

---
+++ 百度地图_多层标记/js/map_point.js
@@ -0,0 +1,28 @@
+﻿var type1_data = [
+    { lng: 120.17514, lat: 30.235249, title: "t1_point1"  },
+    { lng: 120.37714, lat: 30.375182, title: "t1_point2"  },
+ ];
+ 
+var type2_data = [
+{ lng:120.167,lat: 30.2, title: "t2_point1"},
+{ lng:120.278,lat: 30.25, title: "t2_point2"},
+{ lng:120.389,lat: 30.3, title: "t2_point3"},
+{ lng:120.5,lat: 30.35, title: "t2_point4"},
+{ lng:120.111,lat: 30.4, title: "t2_point5"},
+{ lng:120.722,lat: 30.45, title: "t2_point6"},
+{ lng:120.533,lat: 30.5, title: "t2_point7"},
+{ lng:120.444,lat: 30.55, title: "t2_point8"},
+{ lng:120.233,lat: 30.2, title: "t2_point9"},
+{ lng:120.344,lat: 30.25, title: "t2_point10"},
+{ lng:120.177,lat: 30.3, title: "t2_point11"},
+{ lng:120.178,lat: 30.35, title: "t2_point12"},
+{ lng:120.179,lat: 30.4, title: "t2_point13"},
+{ lng:120.18,lat: 30.45, title: "t2_point14"},
+{ lng:120.171,lat: 30.2, title: "t2_point15"},
+{ lng:120.182,lat: 30.25, title: "t2_point16"},
+{ lng:120.169,lat: 30.3, title: "t2_point17"},
+{ lng:120.184,lat: 30.35, title: "t2_point18"},
+{ lng:120.167,lat: 30.4, title: "t2_point19"},
+{ lng:120.186,lat: 30.45, title: "t2_point20"},
+
+];
